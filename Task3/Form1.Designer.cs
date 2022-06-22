53de709fbfce22e42db2e45b72eecb520bd31031
Andrew Nazarov <xiss@users.noreply.github.com>
2022-06-22 23:09:50 +0300

    feat: Task3

---
+++ Task3/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Task3
+{
+    partial class Task2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonRun = new System.Windows.Forms.Button();
+            this.dataGridViewMain = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ButtonRun
+            // 
+            this.ButtonRun.Location = new System.Drawing.Point(19, 403);
+            this.ButtonRun.Name = "ButtonRun";
+            this.ButtonRun.Size = new System.Drawing.Size(286, 41);
+            this.ButtonRun.TabIndex = 0;
+            this.ButtonRun.Text = "Run";
+            this.ButtonRun.UseVisualStyleBackColor = true;
+            this.ButtonRun.Click += new System.EventHandler(this.ButtonRun_Click);
+            // 
+            // dataGridViewMain
+            // 
+            this.dataGridViewMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMain.Location = new System.Drawing.Point(10, 15);
+            this.dataGridViewMain.Name = "dataGridViewMain";
+            this.dataGridViewMain.Size = new System.Drawing.Size(770, 369);
+            this.dataGridViewMain.TabIndex = 1;
+            // 
+            // Task2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridViewMain);
+            this.Controls.Add(this.ButtonRun);
+            this.Name = "Task2";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMain)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonRun;
+        private System.Windows.Forms.DataGridView dataGridViewMain;
+    }
+}
+
---
+++ Task3/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task3
+{
+    public partial class Task2 : Form
+    {
+        public Task2()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonRun_Click(object sender, EventArgs e)
+        {
+            using(Entities entities = new Entities())
+            {
+                dataGridViewMain.DataSource = entities.BuildingSets.ToList();
+            }            
+        }
+    }
+}
